0bd7d072f4e30141d70ff2a8d369c2b1f7462e20
Ashutosh Krishna <ashutoshbritish@gmail.com>
2021-11-28 13:03:18 +0530

    Blog 3 completed

--- GroceryBag/settings.py
+++ GroceryBag/settings.py
@@ -133,3 +133,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/accounts/signin'
